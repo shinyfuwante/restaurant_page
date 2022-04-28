eb4108b163037e3129c9a63b5d77bcd56914e93d
shinyfuwante <mr.philliptang@gmail.com>
2022-04-28 00:13:07 -0700

    Create contact.js

---
+++ src/contact.js
@@ -0,0 +1,32 @@
+import kawasaki_avatar from './assets/pictures/chef_kawasaki.webp';
+
+const mainElement = document.querySelector('div#content');
+console.log(mainElement);
+
+const initializeContacts = () => {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {
+    initializeContacts,
+};
+
