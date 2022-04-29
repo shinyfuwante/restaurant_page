9f2ebadded99902f218aec0d603c4bfd682950ee
shinyfuwante <mr.philliptang@gmail.com>
2022-04-28 21:04:32 -0700

    Introduce tab switching logic on index.js
    This commit introduces the first iteration of tab switching logic on index.js.
    This considers the tabs as buttons and clicking them clears the page, rerenders the buttons,
    then calls the method depending on the button clicked.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,62 @@
 import {initializeHomePage} from './homepage';
 import {initializeMenus} from './menus';
+import {initializeContacts} from './contact';
 
-console.log('heyyyyYYY');
-//initializeHomePage();
-//initializeMenus();
+const mainElement = document.querySelector('div#content');
+
+const tabsListener = (() => {
+    const buttonList = [];
+    let homeButton;
+    let menuButton;
+    let contactButton;
+    const renderButtons = () => {
+        for (let i = 0; i < 3; i++) {
+            const button = document.createElement('button');
+            mainElement.appendChild(button);
+            buttonList[i] = button;
+            
+        }
+        configButtons();
+    };
+
+    const configButtons = () => {
+        homeButton = buttonList[0];
+        homeButton.innerText = "Home";
+        menuButton = buttonList[1];
+        menuButton.innerText = "Menu";
+        contactButton = buttonList[2];
+        contactButton.innerText = "Contact Us";
+    }
+    const listen = () => {
+        homeButton.addEventListener('click', clickButton);
+        menuButton.addEventListener('click', clickButton);
+        contactButton.addEventListener('click', clickButton);
+    }
+
+    const clickButton = (e) => {
+        //clear the page
+        //initialize the correct page
+        clearPage();
+        renderButtons();
+        const button = e.target.innerText;
+        if (button == "Home") initializeHomePage();
+        if (button == "Menu") initializeMenus();
+        if (button == "Contact Us") initializeContacts();
+        listen();
+    }
+    const clearPage = () => {
+        while (mainElement.firstChild) {
+            const element = mainElement.lastChild;
+            element.remove();
+        }
+    }
+        return {
+            listen,
+            renderButtons,
+        };
+})();
+
+
+tabsListener.renderButtons();
+tabsListener.listen();
+initializeHomePage();
