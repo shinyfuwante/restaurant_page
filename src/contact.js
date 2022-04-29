02a59ba192cea89668b1cbbd56bce8b3c7595be3
shinyfuwante <mr.philliptang@gmail.com>
2022-04-28 21:03:47 -0700

    Create basis for contact.js
    This commit creates the basic information for the contact us page. Prior to this, it showed
    nothing.

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,15 @@ const mainElement = document.querySelector('div#content');
 console.log(mainElement);
 
 const initializeContacts = () => {
-    
+    const portrait = document.createElement('img');
+    portrait.src = kawasaki_avatar;
+    mainElement.appendChild(portrait);
+    const text = document.createElement('span');
+    text.innerText = "Meet Chef Kawasaki, the Planet Popstar Chef himself!";
+    mainElement.appendChild(text);
+    const phoneNum = document.createElement('span');
+    phoneNum.innerText = " 555-555-5555";
+    mainElement.appendChild(phoneNum);
 }
 
 
