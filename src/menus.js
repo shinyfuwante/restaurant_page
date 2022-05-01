ac398a89ddaf1d84794f4faa312655d6d672abd7
shinyfuwante <mr.philliptang@gmail.com>
2022-04-30 19:52:54 -0700

    Refactor menu.js to use a single method
    This commit allows menu.js to have a single method to add menu-items to maintain scalability.

--- src/menus.js
+++ src/menus.js
@@ -8,72 +8,59 @@ import './styles/menus.css';
 
 const mainElement = document.querySelector('div#content');
 let container;
-//possibility, throw everything into a json and parse it from there.
+
 const initializeMenus = () => {
     container = document.createElement('div');
     container.classList.add("menu-container");
     mainElement.appendChild(container);
 
-    dededePizza();
-    kirbAuLait();
-    kirbPancake();
-    maximDrink();
-    hayashi();
-    omurice();
+    container.appendChild(createMenuItem(
+        "Dedede Pizza",
+        king3di,
+        "Nicolle style pizza made with pizza kiln enough to the edge apart."
+    ));
+
+    container.appendChild(createMenuItem(
+        "Kirby au Lait",
+        kirbmali,
+        "Café au lait with Kirby marshmallows. You can choose from two marshmallows."
+    ));
+    container.appendChild(createMenuItem(
+        "Kirby Pancake",
+        kirbpcki,
+        "It is a fluffy pancake expressing Kirby pink by adding a puree of Francoise to a blend of fresh cream and custard cream."
+    ));
+    container.appendChild(createMenuItem(
+        "Maximum Tomato Drink",
+        maximi,
+        "It is a pleasant cold soup for hot weather, based on tomatoes and peaches. The \"M\" part was reproduced with balsamico and potatoes."
+    ));
+    container.appendChild(createMenuItem(
+        "Waddledee Hayashi",
+        waddlehayashii,
+        "It is an authentic hayashi rice finished with beef fried in red wine and a demiglace sauce that combines deep richness and mellow flavor."
+    ));
+    container.appendChild(createMenuItem(
+        "Waddledee Omurice",
+        waddleomuricei,
+        "Waddle Dee's omelet rice, to make you slimy. Please enjoy it for a cute sleeping face for a while. (Tottotto egg, Demi saus of rice beet, Rice)"
+    ));
 }
 
-const dededePizza = () => {
+const createMenuItem = (name, image, desc) => {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add("menu-item");
+    const title = document.createElement('h2');
+    title.innerText = name;
+    menuItem.appendChild(title);
     const picture = document.createElement('img');
-    picture.src = king3di;
-    container.appendChild(picture);
+    picture.src = image;
+    menuItem.appendChild(picture);
     const text = document.createElement('span');
-    text.innerText = "Nicolle style pizza made with pizza kiln enough to the edge apart.";
-    container.appendChild(text);
-}
-
-const kirbAuLait = () => {
-    const picture = document.createElement('img');
-    picture.src = kirbmali;
-    container.appendChild(picture);
-    const text = document.createElement('span');
-    text.innerText = "Café au lait with Kirby marshmallows. You can choose from two marshmallows.";
-    container.appendChild(text);
-}
-
-const kirbPancake = () => {
-    const picture = document.createElement('img');
-    picture.src = kirbpcki;
-    container.appendChild(picture);
-    const text = document.createElement('span');
-    text.innerText = "It is a fluffy pancake expressing Kirby pink by adding a puree of Francoise to a blend of fresh cream and custard cream.";
-    container.appendChild(text);
-}
-
-const maximDrink = () => {
-    const picture = document.createElement('img');
-    picture.src = maximi;
-    container.appendChild(picture);
-    const text = document.createElement('span');
-    text.innerText = "It is a pleasant cold soup for hot weather, based on tomatoes and peaches. The \"M\" part was reproduced with balsamico and potatoes.";
-    container.appendChild(text);
-}
-
-const hayashi = () => {
-    const picture = document.createElement('img');
-    picture.src = waddlehayashii;
-    container.appendChild(picture);
-    const text = document.createElement('span');
-    text.innerText = "It is an authentic hayashi rice finished with beef fried in red wine and a demiglace sauce that combines deep richness and mellow flavor.";
-    container.appendChild(text);
-}
-
-const omurice = () => {
-    const picture = document.createElement('img');
-    picture.src = waddleomuricei;
-    container.appendChild(picture);
-    const text = document.createElement('span');
-    text.innerText = "Waddle Dee's omelet rice, to make you slimy. Please enjoy it for a cute sleeping face for a while. (Tottotto egg, Demi saus of rice beet, Rice)";
-    container.appendChild(text);
+    text.innerText = desc;
+    menuItem.appendChild(text);
+    
+    return menuItem;
 }
 
 export {
