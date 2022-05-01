f5169a8ecc1984cd0548bb80c025bf5bd7ba1f97
shinyfuwante <mr.philliptang@gmail.com>
2022-04-30 20:28:31 -0700

    Finish contacts layout

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,5 @@
 import kawasaki_avatar from './assets/pictures/chef_kawasaki.webp';
+import banner from './assets/pictures/banner.webp';
 
 const mainElement = document.querySelector('div#content');
 let container;
@@ -8,8 +9,13 @@ const initializeContacts = () => {
     container.classList.add("contact-container");
     mainElement.appendChild(container);
 
+    const boyos = document.createElement('img');
+    boyos.src = banner;
+    container.append(boyos);
+
     const portrait = document.createElement('img');
     portrait.src = kawasaki_avatar;
+    portrait.classList.add('kawasaki');
     container.appendChild(portrait);
     const text = document.createElement('span');
     text.innerText = "Meet Chef Kawasaki, the Planet Popstar Chef himself!";
@@ -17,6 +23,9 @@ const initializeContacts = () => {
     const phoneNum = document.createElement('span');
     phoneNum.innerText = " 555-555-5555";
     container.appendChild(phoneNum);
+    const email = document.createElement('span');
+    email.innerText = "PopStarChefKawasaki@kirbycafe.jp";
+    container.appendChild(email);
 }
 
 
